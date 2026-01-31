497ed167c88250efe96927804bf2c62e13648df5
Bhaskar2513 <allubhaskar25@gmail.com>
2026-01-31 16:55:16 +0530

    Mysql with python

---
+++ test.py
@@ -0,0 +1,15 @@
+num=int(input("enter a number:"))
+if num % 2== 0:
+    print("even number")
+else:
+    print("odd number")
+
+
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+    
+
